f9321f29c8e67ba6139b51500fee97cd05d763cf
Arthur Davis <arthur.huayaconza.alc@viaexperis.pe>
2023-01-27 19:15:36 -0500

    feat: add print GetHeaders

--- utils/utils.go
+++ utils/utils.go
@@ -2,15 +2,19 @@ package utils
 
 import (
 	"context"
+	"fmt"
 	"google.golang.org/grpc/metadata"
 	"net/http"
 )
 
 func GetHeaders(headers interface{}, hostname string) context.Context {
+	fmt.Println("#######################")
+	fmt.Println("headers: ", headers.(http.Header))
 	headersMap := make(map[string]string)
 	for key, values := range headers.(http.Header) {
 		headersMap[key] = values[0]
 	}
+	fmt.Println("headers: ", headersMap)
 
 	ctx := context.Background()
 
